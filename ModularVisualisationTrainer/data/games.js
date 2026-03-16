02eedf9bf1c3def5690138f66fb2359c8294024e
swri-games-and-quizzes <fighter1.swri@gmail.com>
2026-03-16 17:28:34 +0500

    Delete ModularVisualisationTrainer directory

--- ModularVisualisationTrainer/data/games.js
+++
@@ -1,7 +0,0 @@
-const games=[
-
-`1. d4 d5 2. c4 e6 3. Nc3 Nf6 4. Bg5 Be7 5. e3 O-O 6. Nf3 h6 7. Bh4 c6 8. Bd3 Nbd7 9. O-O`,
-
-`1. e4 e6 2. d4 d5 3. e5 c5 4. c3 Nc6 5. Nf3 Qb6 6. Bd3 Bd7 7. Bc2 Nge7 8. O-O Nf5 9. dxc5 Bxc5`
-
-]
